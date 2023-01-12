4bb26579b3678c2f4d373f07536a342bb45f575e
palle146github <prasanthpalle146@gmail.com>
2023-01-12 12:14:15 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,41 @@
 public class Main {
     static final int SATRTING_POSITION = 0;
+    static final int WINNIG_POSITION = 100;
     static int playerPosition = 0;
-    static int dieRolled(){
-        int diceNumber = (int)(((Math.random() * 10) % 6) + 1);
+
+    static int dieRolled() {
+        int diceNumber = (int) (((Math.random() * 10) % 6) + 1);
         return diceNumber;
     }
-    static int getOption(){
-        int option = (int)(((Math.random() * 10) % 3));
+
+    static int getOption() {
+        int option = (int) (((Math.random() * 10) % 3));
         return option;
     }
+
     public static void main(String[] args) {
-        System.out.println("Snake and Ladder");
-        int diceNumber = dieRolled();
-        System.out.println(" Dice number " + diceNumber);
-        int option = getOption();
-        System.out.println(" Dice number " + option);
-        switch (option){
-            case 0:
-                System.out.println("NO PLAY");
-                break;
-            case 1:
-                System.out.println("LADDER");
-                playerPosition += diceNumber;
-                break;
-            case 2:
-                System.out.println("SNAKE");
-                playerPosition -= diceNumber;
-                break;
+        while (playerPosition != 100) {
+            System.out.println("Snake and Ladder");
+            int diceNumber = dieRolled();
+            System.out.println(" Dice number " + diceNumber);
+            int option = getOption();
+            System.out.println(" Game option " + option);
+            switch (option) {
+                case 0:
+                    System.out.println("NO PLAY");
+                    break;
+                case 1:
+                    System.out.println("LADDER");
+                    playerPosition += diceNumber;
+                    break;
+                case 2:
+                    System.out.println("SNAKE");
+                    playerPosition -= diceNumber;
+                    break;
+            }
+            System.out.println("Player position is " + playerPosition);
         }
+        System.out.println("Final Player Position " + playerPosition);
     }
 }
 
