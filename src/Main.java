cca0c6f10ad4f1e9d402df5587fe2603f1f05b50
palle146github <prasanthpalle146@gmail.com>
2023-01-12 11:32:21 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
+    static final int SATRTING_POSITION = 0;
     public static void main(String[] args) {
         System.out.println("Snake and Ladder");
+        System.out.println("Player Starting Position is "+ SATRTING_POSITION);
     }
 }
